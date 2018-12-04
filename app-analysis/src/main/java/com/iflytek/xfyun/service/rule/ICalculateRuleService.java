4976d753148a7c869d107023cba162ad34af6dbd
hzyuan <hzyuan@iflytek.com>
2018-12-04 20:51:28 +0800

    first commit

--- app-analysis/src/main/java/com/iflytek/xfyun/service/rule/ICalculateRuleService.java
+++
@@ -1,9 +0,0 @@
-package com.iflytek.xfyun.service.rule;
-
-import java.util.List;
-
-import com.iflytek.xfyun.dto.AppActiveDegree;
-
-public interface ICalculateRuleService {
-	List<AppActiveDegree> getAppSore(List<AppActiveDegree> appActiveDegreeList);
-}
---
+++ data-analysis/src/main/java/com/yhz/webmagic/data_analysis/App.java
@@ -0,0 +1,13 @@
+package com.yhz.webmagic.data_analysis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
